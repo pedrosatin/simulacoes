925a4ca0d70141b8288cf6db85c9a0b68c161e39
Pedro Satin <33128737+pedrosatin@users.noreply.github.com>
2026-08-03 19:08:57 -0300

    test(a-vista-vs-parcelado): add tests for SelicAPI.updateSelicDisplay

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,27 @@
+
+if (typeof document !== "undefined") { document.body.innerHTML = `
+  <form id="simulationForm"></form>
+  <input id="productValue">
+  <input id="cashValue">
+  <input id="installments">
+  <div id="results"></div>
+  <span id="selicRate"></span>
+  <span id="selicDate"></span>
+  <span id="cashPayment"></span>
+  <span id="selicInvestment"></span>
+  <span id="cashTotalCost"></span>
+  <span id="installmentValue"></span>
+  <span id="installmentTotal"></span>
+  <span id="installmentTotalCost"></span>
+  <span id="recommendationTitle"></span>
+  <span id="recommendationText"></span>
+  <span id="savingsLabel"></span>
+  <span id="savingsValue"></span>
+  <span id="savingsPercent"></span>
+  <span id="monthlySelicRate"></span>
+  <span id="investmentPeriod"></span>
+  <span id="grossReturn"></span>
+`;
+}
 const utils = require('./js/utils.js')
 global.applyMoneyMask = utils.applyMoneyMask
